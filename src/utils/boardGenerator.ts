5b9ea8fe1eb4f4f7a55ea8132c708f22307ad079
Krisztiaan <krisz011@gmail.com>
2018-08-05 20:14:52 +0200

    add prettier, formatonsave

--- src/utils/boardGenerator.ts
+++ src/utils/boardGenerator.ts
@@ -1,27 +1,28 @@
-import { IBoard } from "../interfaces/board.interface";
+import { IBoard } from '../interfaces/board.interface';
 
 export const generateBoard = (x: number, y: number, bombsCount: number): IBoard => {
+	const board: IBoard = Array(x)
+		.fill([])
+		.map(() => Array(y).fill({}).map(() => ({ isBomb: false, isRevealed: false, hint: 0 })));
 
-  const board: IBoard = Array(x).fill([]).map(()=>Array(y).fill({}).map(() => ({ isBomb: false, isRevealed: false, hint: 0 })))
+	let bombsLeft = bombsCount;
 
-  let bombsLeft = bombsCount
+	let breaker = 10;
 
-  let breaker = 10
+	while (bombsLeft > 0) {
+		let candidate = {
+			x: Math.floor(Math.random() * x),
+			y: Math.floor(Math.random() * y)
+		};
 
-  while(bombsLeft > 0) {
-    let candidate = {
-      x: Math.floor(Math.random() * x),
-      y: Math.floor(Math.random() * y)
-    }
+		if (!board[candidate.x][candidate.y].isBomb) {
+			board[candidate.x][candidate.y].isBomb = true;
+			bombsLeft = bombsLeft - 1;
+		}
 
-    if (!board[candidate.x][candidate.y].isBomb) {
-      board[candidate.x][candidate.y].isBomb = true
-      bombsLeft = bombsLeft - 1
-    }
+		breaker = breaker - 1;
+		if (breaker < 0) break;
+	}
 
-    breaker = breaker - 1
-    if(breaker < 0) break
-  }
-
-  return board
-}
+	return board;
+};

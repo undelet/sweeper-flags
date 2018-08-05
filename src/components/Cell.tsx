cfb310c52a9df7108c77c6205bb95ebe9f9d4968
Krisztiaan <krisz011@gmail.com>
2018-08-05 20:24:23 +0200

    add emoji

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -3,33 +3,30 @@ import { Text, TouchableHighlight, StyleSheet } from 'react-native';
 import { ICell } from '../interfaces/cell.interface';
 
 export interface ICellProps extends ICell {
-  onPress: () => void;
+	onPress: () => void;
 }
 
-export const Cell = ({
-  onPress,
-  isBomb,
-  isRevealed,
-  hint,
-}: ICellProps) => {
-  let text: string | number = '?';
-  if (isRevealed) {
-    if (isBomb) {
-      text = 'O';
-    } else {
-      text = hint;
-    }
-  }
+// 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 💣 🚩 🏳️‍🌈 0️⃣
 
-  return (
-    <TouchableHighlight onPress={onPress} style={styles.container}>
-      <Text>{text}</Text>
-    </TouchableHighlight>
-  );
+export const Cell = ({ onPress, isBomb, isRevealed, hint }: ICellProps) => {
+	let text: string | number = '?';
+	if (isRevealed) {
+		if (isBomb) {
+			text = 'O';
+		} else {
+			text = hint;
+		}
+	}
+
+	return (
+		<TouchableHighlight onPress={onPress} style={styles.container}>
+			<Text>{text}</Text>
+		</TouchableHighlight>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 12,
-  },
+	container: {
+		padding: 12
+	}
 });
